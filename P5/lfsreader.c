548c96d0bffbda028d64278b3edd0cf90efbbf71
Anuhavnidhi <anuhavnidhi@cs.wisc.edu>
2015-04-30 02:16:53 -0500

    Nothing

--- P5/lfsreader.c
+++ P5/lfsreader.c
@@ -14,6 +14,12 @@ void cat(char file[],char path[])
 
 int main(int argc, char* arg[])
 {
+	int i;
+	char cmd[strlen(arg[1])];
+        char path[strlen(arg[2])];
+        char img[strlen(arg[3])];
+       	inodemap *imap[INODEPIECES];
+	// make sure format is right
 	if(argc!=4)
 	{
 		printf("\nError!\\n");
@@ -22,12 +28,26 @@ int main(int argc, char* arg[])
 		printf("\nls    \'absolute pathname for a directory\'   \'lfs image\'");
       		return -1; 
 	}
-	char cmd[strlen(arg[1])];
-	char path[strlen(arg[2])];
-	char img[strlen(arg[3])];
+	// Assign variables values from cmd args
 	strcpy(cmd,arg[1]);
    	strcpy(path,arg[2]);
    	strcpy(img,arg[3]);
+	//open file and create basic lfs structure
+	
+	int lfs=open(img,O_RDONLY);
+	//the first part of fs is the cr region
+	checkpoint *cr=malloc(sizeof(checkpoint));
+	if((read(lfs,cr,sizeof(checkpoint)))<0)
+	{
+		printf("\nError!\\n");
+	}
+	for(i=0;i<INODEPIECES;i++)
+	{
+		
+	}
+
+
+	// check which command is used
 	if(strcmp(cmd,"ls")==0)
 	{
 		ls(img,path);
